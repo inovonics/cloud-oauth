b32ad61455680b78daecbe03a7c33c0f2f5e93fe
Daniel Williams <dwilliams@inovonics.com>
2017-10-23 14:20:02 -0600

    Back to doing some real debugging.

--- inovonics/cloud/oauth/inooauth2provider.py
+++ inovonics/cloud/oauth/inooauth2provider.py
@@ -29,7 +29,7 @@ class InoOAuth2Provider(OAuth2Provider):
     def _clientgetter(self, client_id):
         self.logger.debug("client_id: %s", client_id)
         clients = OAuthClients(self.dstore)
-        client = clients.get(client_id)
+        client = clients.get_by_id(client_id)
         # Client is an object
         return client
 
