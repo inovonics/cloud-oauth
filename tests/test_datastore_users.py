62f0e684bd982b3f82ed736dae919fe0df88adc8
Daniel Williams <dwilliams@inovonics.com>
2017-10-31 11:13:23 -0600

    Fixing unit tests.

--- tests/test_datastore_users.py
+++ tests/test_datastore_users.py
@@ -91,12 +91,12 @@ class TestCasesDatastoreUsers(unittest.TestCase):
 
         # Verify the correct data via direct calls to redis
         ## Check the user_id from the index
-        tmp_user_id = self.dstore.redis.get("oauth:user:{}".format(username)).decode('utf-8')
+        tmp_user_id = self.dstore.redis.get("oauth:users:{}".format(username)).decode('utf-8')
         self.logger.debug("tmp_user_id: %s", tmp_user_id)
-        self.assertEqual(tmp_user_id, user.user_id)
+        self.assertEqual(tmp_user_id, user.oid)
 
         ## Setup the key for the hash
-        tmp_key = "oauth:user{{{}}}".format(tmp_user_id)
+        tmp_key = "oauth:users{{{}}}".format(tmp_user_id)
         self.logger.debug("tmp_key: %s", tmp_key)
 
         ## Check the username
@@ -120,12 +120,12 @@ class TestCasesDatastoreUsers(unittest.TestCase):
         self.assertTrue(pbkdf2_sha512.verify(password, tmp_passhash))
 
         ## Check username is in usernames list
-        tmp_user_in_usernames = self.dstore.redis.sismember("oauth:usernames", username)
+        tmp_user_in_usernames = self.dstore.redis.sismember("oauth:users:usernames", username)
         self.logger.debug("tmp_user_in_usernames: %s", tmp_user_in_usernames)
         self.assertTrue(tmp_user_in_usernames)
 
         ## Check user_id is in user_ids list
-        tmp_user_in_user_ids = self.dstore.redis.sismember("oauth:user_ids", tmp_user_id)
+        tmp_user_in_user_ids = self.dstore.redis.sismember("oauth:users:oids", tmp_user_id)
         self.logger.debug("tmp_user_in_user_ids: %s", tmp_user_in_user_ids)
         self.assertTrue(tmp_user_in_user_ids)
 
@@ -133,7 +133,7 @@ class TestCasesDatastoreUsers(unittest.TestCase):
     def test_update_user(self, username, update_field, update_value):
         self.logger.info("Running test_update_user")
         # Retrieve the user data
-        tmp_user_id = self.dstore.redis.get("oauth:user:{}".format(username)).decode('utf-8')
+        tmp_user_id = self.dstore.redis.get("oauth:users:{}".format(username)).decode('utf-8')
         self.logger.debug("tmp_user_id: %s", tmp_user_id)
         tmp_user = self.db_users.get_by_id(tmp_user_id)
 
@@ -147,16 +147,16 @@ class TestCasesDatastoreUsers(unittest.TestCase):
         self.db_users.update(tmp_user)
 
         # Verify the correct data via direct calls to redis
-        tmp_key = "oauth:user{{{}}}".format(tmp_user_id)
+        tmp_key = "oauth:users{{{}}}".format(tmp_user_id)
         if update_field == "username":
             ## Check the hash, the lookup, and the set
             tmp_username = self.dstore.redis.hget(tmp_key, 'username').decode('utf-8')
             self.logger.debug("tmp_username: %s", tmp_username)
             self.assertEqual(tmp_username, update_value)
-            tmp_id_check = self.dstore.redis.get("oauth:user:{}".format(update_value)).decode('utf-8')
+            tmp_id_check = self.dstore.redis.get("oauth:users:{}".format(update_value)).decode('utf-8')
             self.logger.debug("tmp_id_check: %s", tmp_id_check)
             self.assertEqual(tmp_id_check, tmp_user_id)
-            tmp_in_set = self.dstore.redis.sismember("oauth:usernames", update_value)
+            tmp_in_set = self.dstore.redis.sismember("oauth:users:usernames", update_value)
             self.logger.debug("tmp_in_set: %s", tmp_in_set)
             self.assertTrue(tmp_in_set)
         elif update_field == "password":
@@ -192,17 +192,17 @@ class TestCasesDatastoreUsers(unittest.TestCase):
             for user in default_user_data:
                 # User data will be loaded via direct calls to Redis
                 tmp_user_id = uuid.uuid4()
-                tmp_key = "oauth:user{{{}}}".format(tmp_user_id)
-                pipe.hset(tmp_key, "user_id", tmp_user_id)
+                tmp_key = "oauth:users{{{}}}".format(tmp_user_id)
+                pipe.hset(tmp_key, "oid", tmp_user_id)
                 pipe.hset(tmp_key, "username", user[0])
                 pipe.hset(tmp_key, "password_hash", pbkdf2_sha512.hash(user[1]))
                 pipe.hset(tmp_key, "is_active", user[2])
                 pipe.hset(tmp_key, "scopes", json.dumps(user[3]))
                 # Setup the appropriate references
-                pipe.set("oauth:user:{}".format(user[0]), tmp_user_id)
+                pipe.set("oauth:users:{}".format(user[0]), tmp_user_id)
                 # Load the username and user_id into the appropriate set
-                pipe.sadd("oauth:usernames", user[0])
-                pipe.sadd("oauth:user_ids", tmp_user_id)
+                pipe.sadd("oauth:users:usernames", user[0])
+                pipe.sadd("oauth:users:oids", tmp_user_id)
 
 # === MAIN ===
 if __name__ == '__main__':
