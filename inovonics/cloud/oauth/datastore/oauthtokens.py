50bcd760af43d9fb07472da83710f5ed9885ff21
Daniel Williams <dwilliams@inovonics.com>
2017-11-01 14:24:08 -0600

    I was terrible with renaming things in the last couple of days.

--- inovonics/cloud/oauth/datastore/oauthtokens.py
+++ inovonics/cloud/oauth/datastore/oauthtokens.py
@@ -33,13 +33,13 @@ class OAuthTokens(InoModelBase):
         # Look up the token by the access_token
         with redpipe.autoexec() as pipe:
             oid = pipe.get("oauth:tokens:access:{}".format(access_token))
-        return self.get_by_id(key.result.decode('utf-8'))
+        return self.get_by_id(oid.result.decode('utf-8'))
 
     def get_by_refresh_token(self, refresh_token):
         # Look up the token by the refresh_token
         with redpipe.autoexec() as pipe:
             oid = pipe.get("oauth:tokens:refresh:{}".format(refresh_token))
-        return self.get_by_id(key.result.decode('utf-8'))
+        return self.get_by_id(oid.result.decode('utf-8'))
 
     def create(self, tokens, expiry = 0):
         # If tokens is a singular object, make it a list of one
